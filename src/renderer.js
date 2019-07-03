1882f120f420589b80d58742f8256073aa4ccee5
EshanDias <eash_di@ymail.com>
2019-07-04 03:13:23 +0530

    restructured the project

--- src/renderer.js
+++
@@ -1,9 +0,0 @@
-console.log('Heya, this is coming from a webpack bundle');
-import Vue from 'vue';
-import App from './App.vue';
-
-Vue.config.productionTip = false;
-
-new Vue({
-  render: h => h(App)
-}).$mount('#app');
